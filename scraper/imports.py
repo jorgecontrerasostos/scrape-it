daed7b827f2c10e462a59073c734dc286b5e354d
jorgecontrerasostos <jorge.contreras@justia.com>
2025-02-01 12:34:17 -0600

    chore: clean stuff

--- scraper/imports.py
+++
@@ -1,4 +0,0 @@
-from urllib.parse import urlparse
-from urlib.request import requests
-import sys
-import requests
--- scraper/main.py
+++ scraper/main.py
@@ -2,10 +2,7 @@ from imports import sys
 from helpers import get_domain, get_sitemap
 
 def main() -> None:
-    url = sys.argv[1]
-    domain = get_domain(url)
-    sitemap = get_sitemap(domain)
-    print(sitemap)
-    
+    pass
+
 if __name__ == "__main__":
     main()
